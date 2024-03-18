d2c1ef9baaf13926ebd45a75244ce66b6bb7b625
kumarAjay10 <144784190+kumarAjay10@users.noreply.github.com>
2024-03-18 14:18:21 +0000

    Datatype summary

---
+++ 01_basics/03_datatypesummary.js
@@ -0,0 +1,40 @@
+
+
+// DataTypes 
+/**
+ * primative datatypes (call by value )
+ * 1. string
+ * 2. number 
+ * 3. null 
+ * 4. undefined
+ * 5. Boolean
+ * 6. symbol (for umake unique)
+ * 7. bigInt
+ * 
+ * non primative datatypes (refernce type)
+ * 1. Arrays
+ * 2. object
+ * 3. function
+ * 
+ * 
+ * js is dynamically 
+ */
+
+// arrays 
+
+const heroes = ["shaktiman","naagraj","doga"]
+
+// object 
+
+let myObj = {
+    name:ajay,
+    age:23,
+}
+
+// function
+
+const myfunc = function(){
+  console.log("hello world");
+}
+
+
